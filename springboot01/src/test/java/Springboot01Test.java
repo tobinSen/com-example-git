6968a98051feb1c959fb58b115a68c0b066ef03b
tongjian <jian.tong@b-and-qchina.com>
2019-02-23 15:21:58 +0800

    Merge branch 'master' of https://github.com/apache/rocketmq
    # Conflicts:
    #	.gitignore
    #	README.md
    #	broker/pom.xml
    #	broker/src/main/java/org/apache/rocketmq/broker/BrokerController.java
    #	broker/src/main/java/org/apache/rocketmq/broker/topic/TopicConfigManager.java
    #	broker/src/main/java/org/apache/rocketmq/broker/transaction/queue/TransactionalMessageServiceImpl.java
    #	broker/src/main/java/org/apache/rocketmq/broker/util/ServiceProvider.java
    #	broker/src/test/java/org/apache/rocketmq/broker/BrokerControllerTest.java
    #	broker/src/test/java/org/apache/rocketmq/broker/util/ServiceProviderTest.java
    #	client/pom.xml
    #	client/src/main/java/org/apache/rocketmq/client/consumer/DefaultMQPullConsumer.java
    #	client/src/main/java/org/apache/rocketmq/client/consumer/DefaultMQPushConsumer.java
    #	client/src/main/java/org/apache/rocketmq/client/consumer/MQPullConsumer.java
    #	client/src/main/java/org/apache/rocketmq/client/impl/consumer/DefaultMQPullConsumerImpl.java
    #	client/src/main/java/org/apache/rocketmq/client/impl/consumer/PullAPIWrapper.java
    #	client/src/main/java/org/apache/rocketmq/client/impl/factory/MQClientInstance.java
    #	client/src/main/java/org/apache/rocketmq/client/impl/producer/DefaultMQProducerImpl.java
    #	client/src/main/java/org/apache/rocketmq/client/producer/DefaultMQProducer.java
    #	client/src/test/java/org/apache/rocketmq/client/producer/DefaultMQProducerTest.java
    #	common/pom.xml
    #	common/src/main/java/org/apache/rocketmq/common/BrokerConfig.java
    #	common/src/main/java/org/apache/rocketmq/common/MQVersion.java
    #	common/src/main/java/org/apache/rocketmq/common/MixAll.java
    #	common/src/main/java/org/apache/rocketmq/common/UtilAll.java
    #	common/src/main/java/org/apache/rocketmq/common/constant/DBMsgConstants.java
    #	common/src/test/java/org/apache/rocketmq/common/BrokerConfigTest.java
    #	common/src/test/java/org/apache/rocketmq/common/MixAllTest.java
    #	distribution/pom.xml
    #	example/pom.xml
    #	example/src/main/java/org/apache/rocketmq/example/simple/Producer.java
    #	example/src/main/java/org/apache/rocketmq/example/simple/PullConsumer.java
    #	example/src/main/java/org/apache/rocketmq/example/simple/PullConsumerTest.java
    #	example/src/main/java/org/apache/rocketmq/example/simple/PushConsumer.java
    #	filter/pom.xml
    #	logappender/pom.xml
    #	logging/pom.xml
    #	namesrv/pom.xml
    #	openmessaging/pom.xml
    #	pom.xml
    #	remoting/pom.xml
    #	remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingAbstract.java
    #	remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingClient.java
    #	remoting/src/main/java/org/apache/rocketmq/remoting/netty/NettyRemotingServer.java
    #	srvutil/pom.xml
    #	store/pom.xml
    #	store/src/main/java/org/apache/rocketmq/store/config/MessageStoreConfig.java
    #	test/pom.xml
    #	test/src/test/java/org/apache/rocketmq/test/base/BaseConf.java
    #	test/src/test/java/org/apache/rocketmq/test/client/consumer/filter/SqlFilterIT.java
    #	tools/pom.xml
    #	tools/src/main/java/org/apache/rocketmq/tools/command/MQAdminStartup.java
    #	tools/src/main/java/org/apache/rocketmq/tools/command/consumer/ConsumerProgressSubCommand.java

--- springboot01/src/test/java/Springboot01Test.java
+++ springboot01/src/test/java/Springboot01Test.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,8 +15,14 @@ public class Springboot01Test {
     @Autowired
     private Dog dog;
 
+    //【注意】 这个是容器启动后自行进行创建的
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Test
     public void test1() {
+        System.out.println(dog.toString());
+        Dog dog = applicationContext.getBean(Dog.class);
         System.out.println(dog.toString());
     }
 }
