63ebbcd40c3b66d2f8a29b02e5d39927aa2a6260
tongjian <jian.tong@b-and-qchina.com>
2019-05-09 22:14:48 +0800

    ces

--- springboot01/src/main/java/com/uplooking/sentinel_apollo/FlowRuleApolloProvid.java
+++ springboot01/src/main/java/com/uplooking/sentinel_apollo/FlowRuleApolloProvid.java
@@ -3,10 +3,6 @@ package com.uplooking.sentinel_apollo;
 public class FlowRuleApolloProvid  {
 
     public static void main(String[] args) {
-        System.out.println("123");
-        System.out.println("张三");
-        System.out.println("历史");
-        System.out.println("--------------->");
-        System.out.println("余氯竞争");
+
     }
 }
