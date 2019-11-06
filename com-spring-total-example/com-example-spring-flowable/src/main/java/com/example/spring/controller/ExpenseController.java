4a998dcb03a64e6b77f5066105a5ceb42a0fe3d7
tongjian <jian.tong@b-and-qchina.com>
2019-11-06 16:56:32 +0800

    fix

---
+++ com-spring-total-example/com-example-spring-flowable/src/main/java/com/example/spring/controller/ExpenseController.java
@@ -0,0 +1,92 @@
+package com.example.spring.controller;
+
+import org.flowable.engine.ProcessEngine;
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.task.api.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("expense")
+public class ExpenseController {
+
+    @Autowired
+    private RuntimeService runtimeService;
+    @Autowired
+    private TaskService taskService;
+    @Autowired
+    private RepositoryService repositoryService;
+    @Autowired
+    private ProcessEngine processEngine;
+
+    /**
+     * 添加报销
+     *
+     * @param userId    用户Id
+     * @param money     报销金额
+     * @param descption 描述
+     */
+    @RequestMapping(value = "add")
+    @ResponseBody
+    public String addExpense(String userId, Integer money, String descption) {
+        //启动流程
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("taskUser", userId);
+        map.put("money", money);
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("Expense", map);
+        return "提交成功.流程Id为：" + processInstance.getId();
+    }
+
+    /**
+     * 获取审批管理列表
+     */
+    @RequestMapping(value = "/list")
+    @ResponseBody
+    public Object list(String userId) {
+        List<Task> tasks = taskService.createTaskQuery().taskAssignee(userId).orderByTaskCreateTime().desc().list();
+        for (Task task : tasks) {
+            System.out.println(task.toString());
+        }
+        return tasks.toArray().toString();
+    }
+
+    /**
+     * 批准
+     *
+     * @param taskId 任务ID
+     */
+    @RequestMapping(value = "apply")
+    @ResponseBody
+    public String apply(String taskId) {
+        Task task = taskService.createTaskQuery().taskId(taskId).singleResult();
+        if (task == null) {
+            throw new RuntimeException("流程不存在");
+        }
+        //通过审核
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("outcome", "通过");
+        taskService.complete(taskId, map);
+        return "processed ok!";
+    }
+
+    /**
+     * 拒绝
+     */
+    @ResponseBody
+    @RequestMapping(value = "reject")
+    public String reject(String taskId) {
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("outcome", "驳回");
+        taskService.complete(taskId, map);
+        return "reject";
+    }
+
+}
