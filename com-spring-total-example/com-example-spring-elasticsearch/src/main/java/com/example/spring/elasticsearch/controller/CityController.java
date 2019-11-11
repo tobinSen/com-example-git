16e3f73baa1d4da32db5f4cae1f5d125bc08550c
tongjian <jian.tong@b-and-qchina.com>
2019-11-11 22:53:16 +0800

    fix

--- com-spring-total-example/com-example-spring-elasticsearch/src/main/java/com/example/spring/elasticsearch/controller/CityController.java
+++ com-spring-total-example/com-example-spring-elasticsearch/src/main/java/com/example/spring/elasticsearch/controller/CityController.java
@@ -3,6 +3,7 @@ package com.example.spring.elasticsearch.controller;
 import com.example.spring.elasticsearch.dao.CityRepository;
 import com.example.spring.elasticsearch.domain.City;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,8 @@ public class CityController {
 
     @Autowired
     private CityRepository cityRepository;
+    @Autowired
+    private ElasticsearchTemplate elasticsearchTemplate;
 
     @PostMapping(value = "api/city.do")
     public City save(@RequestBody City city) throws Exception {
---
+++ com-spring-total-example/com-example-spring-netty/src/main/java/com/example/spring/netty/nio/example1.java
@@ -0,0 +1,27 @@
+package com.example.spring.netty.nio;
+
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ *
+ */
+public class example1 {
+
+    public static void main(String[] args) throws Exception {
+        String str = "hello,nio,我是博学谷";
+        FileOutputStream fos = new FileOutputStream("basic.txt");
+        //通道
+        FileChannel fc = fos.getChannel();
+        //缓冲区
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        buffer.put(str.getBytes());
+        //将缓冲区中的position置为0
+        buffer.flip();
+        //把缓冲区的数据写到通道里
+        fc.write(buffer);
+        fos.close();
+    }
+
+}
