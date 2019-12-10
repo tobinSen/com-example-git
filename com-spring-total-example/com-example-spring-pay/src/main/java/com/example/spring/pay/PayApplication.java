2024259ca160b3eb4f3da419103a43b4d95d9577
tongjian <1245007716@qq.com>
2019-12-10 23:27:19 +0800

    数字签名 + 数字证书

---
+++ com-spring-total-example/com-example-spring-pay/src/main/java/com/example/spring/pay/PayApplication.java
@@ -0,0 +1,31 @@
+package com.example.spring.pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * 公钥 <--> 私钥
+ *
+ * client （公钥加密 + 文件）--->  server (私钥解密)
+ *
+ * client （公钥解密）     <---文件->摘要（私钥加密）-->数字签名
+ *
+ * client （浏览器中证书中心进行匹配  <---  文件->摘要（私钥加密）-->数字签名 + （数字证书中心用私钥对公钥做加密）->数字证书
+ *
+ *
+ * 问题1：
+ *  client 使用的不是server提供的公钥，client并不知道公钥是否是server端发送的
+ *
+ *
+ */
+
+@SpringBootApplication
+public class PayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class,args);
+    }
+
+
+}
