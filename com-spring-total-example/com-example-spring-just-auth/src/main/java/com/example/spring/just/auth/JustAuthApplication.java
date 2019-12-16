cedb3a4910b38fea1547795619af16d093827146
tongjian <jian.tong@b-and-qchina.com>
2019-12-16 16:39:26 +0800

    justAuth

---
+++ com-spring-total-example/com-example-spring-just-auth/src/main/java/com/example/spring/just/auth/JustAuthApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring.just.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustAuthApplication.class, args);
+    }
+}
