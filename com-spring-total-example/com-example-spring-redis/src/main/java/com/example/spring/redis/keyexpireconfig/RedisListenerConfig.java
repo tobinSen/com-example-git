22756e67a2408200b4157bbdcd9b99b6d5ea19f0
tongjian <jian.tong@b-and-qchina.com>
2019-12-23 22:32:22 +0800

    KeyExpirationEventMessageListener

---
+++ com-spring-total-example/com-example-spring-redis/src/main/java/com/example/spring/redis/keyexpireconfig/RedisListenerConfig.java
@@ -0,0 +1,22 @@
+package com.example.spring.redis.keyexpireconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+/**
+ *   PUBLISH __keyspace@0__:sampleKey del :订阅指定key的所有操作
+ *   PUBLISH __keyevent@0__:del sampleKey :订阅指定操作的key
+ */
+@Configuration
+public class RedisListenerConfig {
+    @Bean
+    RedisMessageListenerContainer container(RedisConnectionFactory redisConnectionFactory) {
+
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(redisConnectionFactory);
+//        container.addMessageListener(new RedisExpiredListener(), new PatternTopic("__keyevent@0__:expired"));
+        return container;
+    }
+}
