13729102d76f838957eb4b6d9bdec455e800f492
tongjian <1245007716@qq.com>
2020-04-18 20:57:09 +0800

    spring.profiles.active

---
+++ com-spring-total-example/com.example.spring.profile/src/main/java/com/example/spring/profile/ProfileConfig.java
@@ -0,0 +1,32 @@
+package com.example.spring.profile;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 1.当我文件内有相同的字段的时候，是基于激活【文件的来进行覆盖】的和【环境的覆盖】
+ * 2.--spring.profiles.active=dev 的优先级大于application.yml中的优先级
+ * 总结：4，3
+ */
+@Configuration
+public class ProfileConfig {
+
+    @Value("${id}")
+    private Integer id;
+
+    @Value("${name}")
+    private String name;
+
+    @Value("${age}")
+    private Integer age;
+
+//    @Value("${address}")
+//    private String address;
+
+    @PostConstruct
+    public void init() {
+        System.out.println(id + "--" + name + "--" + age + "--" );
+    }
+}
