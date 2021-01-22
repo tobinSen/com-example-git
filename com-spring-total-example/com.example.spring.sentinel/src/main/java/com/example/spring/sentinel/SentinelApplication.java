379fc3c261441cdd1c912f8f5dc662c7f1035b3a
童健 <tongjian2@sfmail.sf-express.com>
2021-01-22 16:09:26 +0800

    init

--- com-spring-total-example/com.example.spring.sentinel/src/main/java/com/example/spring/sentinel/SentinelApplication.java
+++ com-spring-total-example/com.example.spring.sentinel/src/main/java/com/example/spring/sentinel/SentinelApplication.java
@@ -9,4 +9,20 @@ public class SentinelApplication {
     public static void main(String[] args) {
         SpringApplication.run(SentinelApplication.class, args);
     }
+
+    /**
+     *                                    ProcessorSlot
+     *
+     *                                  AbstractLinkedProcessorSlot  (next)
+     *
+     *         ProcessorSlotChain           NodeSelectorSlot  ClusterBuilderSlot  LogSlot  StatisticSlot  SystemSlot  AuthoritySlot  FlowSlot  DegradeSlot
+     *                                         (DefaultNode)     (ClusterNode)
+     *      DefaultProcessorSlotChain (first  <--> end)
+     *
+     *
+     *  first -> NodeSelectorSlot ->  ClusterBuilderSlot  -> LogSlot -> StatisticSlot -> SystemSlot -> AuthoritySlot -> FlowSlot -> DegradeSlot -> end
+     *
+     *
+     * //总结： 5 8 3 点
+     */
 }
