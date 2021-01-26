51d2a1c6103b64e55c85fa1742962bf8e2d204a9
tongjian <1245007716@qq.com>
2021-01-26 12:53:35 +0800

    update

--- com-spring-total-example/com-example-spring-gateway/src/main/java/com/example/spring/gateway/configuration/GatewayConfiguration.java
+++ com-spring-total-example/com-example-spring-gateway/src/main/java/com/example/spring/gateway/configuration/GatewayConfiguration.java
@@ -1,9 +1,6 @@
 package com.example.spring.gateway.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,9 +11,9 @@ public class GatewayConfiguration {
     @Value("${spring.route.uri}")
     private String uri;
 
-    @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
-        return builder.routes().route(r -> r.path(path).uri(uri)).build();
-    }
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+//        return builder.routes().route(r -> r.path(path).uri(uri)).build();
+//    }
 
 }
