6259f916d0e1ba0632e2f9ae8b575c69698fb905
tongjian <1245007716@qq.com>
2021-02-04 12:23:43 +0800

    update

--- com-spring-total-example/com-example-spring-git/src/main/java/com/example/spring/git/GitApplication.java
+++ com-spring-total-example/com-example-spring-git/src/main/java/com/example/spring/git/GitApplication.java
@@ -8,6 +8,17 @@ import org.springframework.core.annotation.Order;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+/**
+ * 场景：代码commit到本地仓库，还没有push到远程仓库，这时要回退代码。
+ *
+ * 介绍下Reset Head中三种Reset Type类型：
+ *
+ * 1.Mixed（默认）：它回退到某个版本，本地会保留源码，回退commit和index信息(add 信息)，若要提交重新commit。
+ *
+ * 2.soft: 回退到某个版本，只回退了commit的信息，不会恢复到index file一级，若要提交重新commit。
+ *
+ * 3.Hard:彻底回退到某个版本，本地的源码也会变为上一个版本的内容。
+ */
 @SpringBootApplication
 @EnableAsync
 public class GitApplication {
