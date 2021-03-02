1092ae02cb0e0fcdcf3530d10a3f02dec78d3a82
童健 <tongjian2@sfmail.sf-express.com>
2021-03-02 17:11:48 +0800

    time

--- com-spring-total-example/com.exmaple.sping.web/src/main/java/com/example/spring/web/circulation/circulationApplication.java
+++ com-spring-total-example/com.exmaple.sping.web/src/main/java/com/example/spring/web/circulation/circulationApplication.java
@@ -2,6 +2,11 @@ package com.example.spring.web.circulation;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 /**
  * ClassPathXmlApplicationContext
  * AnnotationConfigApplicationContext
@@ -15,5 +20,37 @@ public class circulationApplication {
 
         ManagerA managerA = (ManagerA)context.getBean("managerA");
         Thread.currentThread().join();
+
+
+        //  new Timer() 需要等待上次任务执行完后才会进行下个任务的运行
+        //  scheduleAtFixedRate 上一个任务还未执行完到了下个任务的执行时间会立刻执行
+        // scheduleWithFixedDelay 等待上一个任务执行完后，在等待时间间隔后在进行执行
+//        new Timer().scheduleAtFixedRate(new TimerTask() {
+//                                            @Override
+//                                            public void run() {
+//                                                System.out.println("任务开始 time: " + LocalDateTime.now());
+//                                                try {
+//                                                    Thread.sleep(5000);
+//                                                } catch (InterruptedException e) {
+//                                                    e.printStackTrace();
+//                                                }
+//                                                System.out.println("任务结束 time: " + LocalDateTime.now());
+//
+//                                            }
+//                                        }, 2000, 3000);
+
+            ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(2);
+            scheduledThreadPool.scheduleWithFixedDelay(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println("任务开始 time: " + LocalDateTime.now());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("任务结束 time: " + LocalDateTime.now());
+                }
+            }, 2000, 3000, TimeUnit.MILLISECONDS);
     }
 }
